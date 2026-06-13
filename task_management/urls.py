f376cd2a5a9bab92119b1ec81617a8bb9f7eb2a3
istiak ahmmed <istiakah32@gmail.com>
2026-06-13 12:10:28 +0600

    another update dashboard

--- task_management/urls.py
+++ task_management/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,manager_dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('manager-dashboard/', manager_dashboard),
 ]
