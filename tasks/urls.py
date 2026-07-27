f0346bbb30d1914c34606e8341c0aa49495c45e6
istiak ahmmed <istiakah32@gmail.com>
2026-07-27 16:13:28 +0600

    fix bug

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,15 +1,20 @@
-from turtle import update
-
-from django.tasks import task
 from django.urls import path
-from tasks.views import delete_task, manager_dashboard, user_dashboard, test,create_task, view_task, update_task, delete_task
+from tasks.views import (
+    manager_dashboard,
+    user_dashboard,
+    test,
+    create_task,
+    view_task,
+    update_task,
+    delete_task,
+)
 
 urlpatterns = [
-    path("manager-dashboard/", manager_dashboard, name='manager-dashboard'),
-    path("user-dashboard/", user_dashboard),
-    path("test/", test),
-    path("create-task/", create_task, name='create-task'),
-    path("view_task/", view_task),
-    path("update-task/<int:id>/", update_task, name='update-task'),
-    path("delete-task/<int:id>/", delete_task, name='delete-task'),
+    path("manager-dashboard/", manager_dashboard, name="manager-dashboard"),
+    path("user-dashboard/", user_dashboard, name="user-dashboard"),
+    path("test/", test, name="test"),
+    path("create-task/", create_task, name="create-task"),
+    path("view-task/", view_task, name="view-task"),
+    path("update-task/<int:id>/", update_task, name="update-task"),
+    path("delete-task/<int:id>/", delete_task, name="delete-task"),
 ]
